03c1b0ec3e01393d565eb8c88d3f9e7e720088e5
Rish <rish@users.noreply.github.com>
2015-04-27 22:16:52 +0100

    Add basic browser-sync task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var bs = require('browser-sync').create();
 
-gulp.task('default', ['server'], function() {
+gulp.task('default', ['server', 'browser-sync'], function() {
 });
 
 gulp.task('server', function() {
   var server = gls('server.js');
   server.start();
 })
+
+gulp.task('browser-sync', function() {
+  bs.init({
+    proxy: "http://localhost:1337"
+  });
+});
