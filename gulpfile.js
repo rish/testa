683ff0711ac2276726fe69b8b9e8b6359f1b8dda
Rish <rish@users.noreply.github.com>
2015-04-27 22:19:58 +0100

    Add a TODO to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var gls = require('gulp-live-server');
 var bs = require('browser-sync').create();
 
+// TODO
+// Set up a seperate server for the view layer and have server.js as
+// a pure service layer that connects to the RethinkDB driver.
+// The view layer will take advantage of Browser Sync's awesomeness
+// Possibly use nodemon or pm2 to run the service layer
+
 gulp.task('default', ['server', 'browser-sync'], function() {
 });
 
