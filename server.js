e0d37d6800cd76681efb58800cbd5e4394cefa2e
Rish <rish@users.noreply.github.com>
2015-04-28 22:58:54 +0100

    Stuff a basic query for RethinkDB

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var r = require('rethinkdb');
 
 app.get('/', function(req, res) {
+
+  var conn = r.connect(function(err, conn) {
+    if(err) {
+      console.log("Connection error");
+      console.log(err);
+    } else {
+      console.log("Connection successful");
+      r.dbList().run(conn, function(err, cursor) {
+        console.log(cursor);
+      });
+    }
+  });
+
   res.send('Nope ... ');
+
 });
 
 app.listen(1337);
